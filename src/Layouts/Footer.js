53d115ecce57f63b8becb44481a2637cfd6026be
Mustafa <80291704+kahramanX@users.noreply.github.com>
2022-02-01 03:00:18 +0300

    added footer section

--- src/Layouts/Footer.js
+++ src/Layouts/Footer.js
@@ -1,8 +1,18 @@
 // SASS
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "../Assets/Styles/footer.scss";
 
 function Footer() {
-  return <footer>FOOTER</footer>;
+  return (
+    <Router>
+      <footer>
+        <div className="ft-direction-container">
+          <span>Burası sıkıcı, filtreleme özelliğini kullan :)</span>
+          <Link to="filter">Filtrele</Link>
+        </div>
+      </footer>
+    </Router>
+  );
 }
 
 export default Footer;
