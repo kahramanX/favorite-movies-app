8670dd9f1c7bdb034ae8a3706f6c1d7d68e5353a
Mustafa <80291704+kahramanX@users.noreply.github.com>
2022-02-08 16:33:33 +0300

    added movie path in searchbox

--- src/components/SearchSectionOnHeader/SearchMovie.jsx
+++ src/components/SearchSectionOnHeader/SearchMovie.jsx
@@ -1,16 +1,22 @@
+import { Link } from "react-router-dom";
+
 function SearchMovie({ movieInfos }) {
+  let movieUrl = `${movieInfos.id}/${movieInfos.title}`;
+
   return (
-    <div className="movie-item">
-      <div className="movie-image">
-        <img
-          src={`https://image.tmdb.org/t/p/original/${
-            movieInfos.poster_path && movieInfos.poster_path
-          }`}
-          alt="Mini_Movie_Photo"
-        />
+    <Link to={`/movie/${movieUrl.replace(/ /g, "-")}`}>
+      <div className="movie-item">
+        <div className="movie-image">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${
+              movieInfos.poster_path && movieInfos.poster_path
+            }`}
+            alt="Mini_Movie_Photo"
+          />
+        </div>
+        <div className="movie-name">{movieInfos.title && movieInfos.title}</div>
       </div>
-      <div className="movie-name">{movieInfos.title && movieInfos.title}</div>
-    </div>
+    </Link>
   );
 }
 
